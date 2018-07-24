3ab62f01e6c1091d9c7f82d8e2eb80d862d1480d
santiagonoguez <santiago@nul-unu.com>
2018-07-24 15:37:14 -0500

    upgrade to Circle CI 2

--- upload-dist.sh
+++ upload-dist.sh
@@ -1,7 +1,3 @@
-git clone git@github.com:Rise-Vision/private-keys.git
-mv private-keys ..
-gcloud auth activate-service-account 452091732215@developer.gserviceaccount.com --key-file ../private-keys/storage-server/rva-media-library-ce0d2bd78b54.json
-
 cp -r dist spreadsheet
 gsutil -m cp -r spreadsheet gs://install-versions.risevision.com/widgets/
 gsutil -m acl -r ch -u AllUsers:R gs://install-versions.risevision.com/widgets/spreadsheet
