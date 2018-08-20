4b0a19c132de865a4b1270091c8c3bc4849ec579
santiagonoguez <santiago@nul-unu.com>
2018-08-20 10:37:54 -0500

    include target directory

--- upload-dist.sh
+++ upload-dist.sh
@@ -1,4 +1,4 @@
 cp -r dist spreadsheet
-gsutil rsync -d -r spreadsheet gs://install-versions.risevision.com/widgets/
+gsutil rsync -d -r spreadsheet gs://install-versions.risevision.com/widgets/spreadsheet
 gsutil -m acl -r ch -u AllUsers:R gs://install-versions.risevision.com/widgets/spreadsheet
 gsutil -m setmeta -r -h Cache-Control:private,max-age=0 gs://install-versions.risevision.com/widgets/spreadsheet
