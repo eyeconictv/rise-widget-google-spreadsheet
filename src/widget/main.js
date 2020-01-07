c094ea9f940abe43e50554afa36c963dc4e3d9f3
Stuart Lees <stuartlees@gmail.com>
2020-01-07 17:22:36 -0500

    Simplify

--- src/widget/main.js
+++ src/widget/main.js
@@ -7,29 +7,13 @@ const React = require( "react" ),
 ( function( window, document ) {
   "use strict";
 
-  var started = false;
-
   // Disable context menu (right click menu)
   window.oncontextmenu = function() {
     return false;
   };
 
-  function start() {
-    if ( !started ) {
-      started = true;
-      ReactDOM.render( <Main />, document.getElementById( "mainContainer" ) );
-    }
-  }
-
   window.addEventListener( "WebComponentsReady", function() {
-    // At this point we are guaranteed that all required polyfills have loaded,
-    // all HTML imports have loaded, and all defined custom elements have upgraded
-    console.log( "web components ready!" ); // eslint-disable-line no-console
-    start();
+    ReactDOM.render( <Main />, document.getElementById( "mainContainer" ) );
   } );
 
-  setTimeout( function() {
-    console.log( "delayed start attempt" ); // eslint-disable-line no-console
-    start();
-  }, 5000 );
 } )( window, document );
